21b6226cf398aa926b31c78c31b410b657d113cf
mar3 <irisawa.masaru@gmail.com>
2014-07-22 16:19:22 +0900

    wip

---
+++ code.example/project.test1/app1/templatetags/nbsp.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+#
+# “ÆŽ©ƒtƒBƒ‹ƒ^‚ÌŽÀ‘•—á
+#
+
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter()
+def nbsp(value):
+	if value == None:
+		return 'None'
+	return mark_safe(value.replace(' ', '&nbsp;'))
