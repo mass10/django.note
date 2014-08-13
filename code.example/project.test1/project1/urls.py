753a7e7b78966847c8bfe9cf830e050ed4670493
mar3 <irisawa.masaru@gmail.com>
2014-08-13 11:59:09 +0900

    wip

--- code.example/project.test1/project1/urls.py
+++ code.example/project.test1/project1/urls.py
@@ -19,4 +19,7 @@ urlpatterns = patterns('',
 	url(r'^filters/show$', 'app1.filters.views.show', name='show'),
 	url(r'^listeners/show$', 'app1.listeners.views.show', name='show'),
 	url(r'^users/show$', 'app1.users.views.show', name='show'),
+	url(r'^users/add$', 'app1.users.views.add', name='add'),
+	url(r'^users/add_complete$', 'app1.users.views.add_complete', name=''),
+	url(r'^users/del$', 'app1.users.views.del', name='del'),
 )
