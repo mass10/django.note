95d3340a221f3cf7e0b39033889051284432994c
mar3 <irisawa.masaru@gmail.com>
2014-08-22 18:03:53 +0900

    wip

--- code.example/project.test1/app1/models.py
+++ code.example/project.test1/app1/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Person(models.Model):
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    mail = models.CharField(max_length=1000)
+
+
