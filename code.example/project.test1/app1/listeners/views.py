285b1f5d7597dd7833d827416d671f584dd877b6
mar3 <irisawa.masaru@gmail.com>
2014-09-09 18:44:18 +0900

    wip

--- code.example/project.test1/app1/listeners/views.py
+++ code.example/project.test1/app1/listeners/views.py
@@ -5,6 +5,7 @@ import logging
 import subprocess
 import time
 import inspect
+import datetime
 from app1.utils import *
 from django.contrib.auth.decorators import login_required
 
@@ -13,17 +14,9 @@ logger = logging.getLogger(__name__)
 @login_required
 def show(request):
 
-	# *************************************************************************
-	# *************************************************************************
-	# *************************************************************************
-	#
-	#
-	# netstat の状態を表示するアクション
-	#
-	#
-	# *************************************************************************
-	# *************************************************************************
-	# *************************************************************************
+	###########################################################################
+	# netstat の状態を表示するビュー
+	###########################################################################
 
 	logger.info('<' + __name__ + '.' + inspect.getframeinfo(inspect.currentframe()).function + '()> $$$ start $$$');
 
@@ -34,17 +27,42 @@ def show(request):
 	# =========================================================================
 	# validation	
 	# =========================================================================	
-	# if False == util.validate_session(request):
-	# 	logger.debug(u'トップページへリダイレクトします。')
-	# 	logger.info('<' + __name__ + '.' + inspect.getframeinfo(inspect.currentframe()).function + '()> --- end ---');
-	# 	return django.http.HttpResponseRedirect('/')
 
 	# =========================================================================
 	# process
 	# =========================================================================
 
-	# current user
-	user_name = request.session.get('user')
+	# =========================================================================
+	# contents
+	# =========================================================================
+	fields = {}
+	util.fill_menu_items(request, fields)
+	context = django.template.RequestContext(request, fields)
+	template = django.template.loader.get_template('listeners/show.html')
+	logger.info('<' + __name__ + '.' + inspect.getframeinfo(inspect.currentframe()).function + '()> --- end ---');
+	return django.http.HttpResponse(template.render(context))
+
+@login_required
+def content(request):
+
+	###########################################################################
+	# netstat の状態を表示するビュー
+	###########################################################################
+
+	logger.info('<' + __name__ + '.' + inspect.getframeinfo(inspect.currentframe()).function + '()> $$$ start $$$');
+
+	# =========================================================================
+	# setup	
+	# =========================================================================	
+
+	# =========================================================================
+	# validation	
+	# =========================================================================	
+
+	# =========================================================================
+	# process
+	# =========================================================================
+
 	# netstat の設定をロード
 	listeners = util.listeners_list()
 
@@ -52,11 +70,12 @@ def show(request):
 	# contents
 	# =========================================================================
 	fields = {}
-	fields['form'] = {
+	fields['c'] = {
 		'listeners': listeners,
+		'current_timestamp': datetime.datetime.now(),
 	}
 	util.fill_menu_items(request, fields)
 	context = django.template.RequestContext(request, fields)
-	template = django.template.loader.get_template('listeners/show.html')
+	template = django.template.loader.get_template('listeners/content.html')
 	logger.info('<' + __name__ + '.' + inspect.getframeinfo(inspect.currentframe()).function + '()> --- end ---');
 	return django.http.HttpResponse(template.render(context))
