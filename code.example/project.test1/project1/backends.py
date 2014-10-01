ca708a2cf2341cb1f339e90ab92de552ba2ffd66
mar3 <irisawa.masaru@gmail.com>
2014-10-01 17:42:40 +0900

    wip

---
+++ code.example/project.test1/project1/backends.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+#
+#
+# 独自認証の実装サンプル
+#
+#
+
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.models import User
+
+class EmailLoginModel(ModelBackend):
+
+	def authenticate(self, email=None, password=None, **kwargs):
+		try:
+			user = User.objects.get(email=email)
+			if user.check_password(password):
+				return user
+			return None
+		except User.DoesNotExist:
+			return None
