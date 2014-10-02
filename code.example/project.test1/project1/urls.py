8669767dcc2499fafb1cc7fd5dcc89b0774c390e
mar3 <irisawa.masaru@gmail.com>
2014-10-02 18:01:22 +0900

    wip

--- code.example/project.test1/project1/urls.py
+++ code.example/project.test1/project1/urls.py
@@ -73,5 +73,6 @@ urlpatterns = patterns('',
 	url(r'^images/$', 'app1.images.views.default'),
 	url(r'^images/save$', 'app1.images.views.save'),
 	url(r'^images/thumbnails$', 'app1.images.views.thumbnails'),
+	url(r'^images/thumbnail$', 'app1.images.views.thumbnail'),
 	# url(r'^images/$', images.as_view()),
 )
